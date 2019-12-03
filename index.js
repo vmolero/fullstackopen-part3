cac25da18f7da7bf33a0ca007f85063416998602
Victor Molero <victor.molero@kuveno.com>
2019-12-03 20:46:45 +0100

    Phonebook backend step7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan("tiny"));
 
 const PORT = 3001;
 let persons = [
