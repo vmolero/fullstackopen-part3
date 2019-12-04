6e74b8906519f4ab254601bdcef3a4c430057767
Victor Molero <victor.molero@kuveno.com>
2019-12-04 12:31:42 +0100

    3.12 Command-line database

---
+++ mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+let name = null;
+let number = null;
+if (3 in process.argv) {
+  name = process.argv[3];
+}
+if (4 in process.argv) {
+  number = process.argv[4];
+}
+
+const url = `mongodb+srv://fullstack:${password}@cluster0-qoe6q.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (!!name && !!number) {
+  const person = new Person({
+    name,
+    number
+  });
+
+  person.save().then(response => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then(result => {
+    console.log("phonebook:");
+    result.forEach(person => {
+      console.log(person.name + " " + person.number);
+    });
+    mongoose.connection.close();
+  });
+}
