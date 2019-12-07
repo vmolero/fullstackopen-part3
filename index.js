225eb1782d164037a24f13280c50f6b518234329
Victor Molero <victor.molero@gmail.com>
2019-12-07 14:40:10 +0100

    3.16: Phonebook database, step3 (bis)

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const Person = require("./models/person");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-
 app.use(express.static("build"));
+app.use(bodyParser.json());
 
 function isEmptyObject(obj) {
   return Object.keys(obj).length === 0 && obj.constructor === Object;
@@ -38,14 +37,16 @@ app.get("/api/persons", (request, response) => {
   return Person.findAll().then(people => response.json(people));
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  return Person.find(id).then(person => {
-    if (person) {
-      return response.json(person);
-    }
-    return response.status(404).send({ error: "Not found" });
-  });
+  return Person.find(id)
+    .then(person => {
+      if (person) {
+        return response.json(person);
+      }
+      return response.status(404).send({ error: "Not found" });
+    })
+    .catch(err => next(err));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -71,12 +72,12 @@ app.put("/api/persons/:id", (request, response) => {
   return response.sendStatus(400);
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   return Person.findByIdAndRemove(request.params.id)
     .then(result => {
       return response.status(204).end();
     })
-    .catch(error => response.status(404).end());
+    .catch(error => next(err));
 });
 
 app.get("/info", (request, response) => {
@@ -86,6 +87,24 @@ app.get("/info", (request, response) => {
     return response.send(info);
   });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
